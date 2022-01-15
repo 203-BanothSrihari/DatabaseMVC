fe73b90233c23c26abdf80dfc0681fd192154ae6
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 17:08:39 +0530

    Add project files.

---
+++ DatabaseMVC/Data/DatabaseMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DatabaseMVC.Models;
+
+namespace DatabaseMVC.Data
+{
+    public class DatabaseMVCContext : DbContext
+    {
+        public DatabaseMVCContext (DbContextOptions<DatabaseMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DatabaseMVC.Models.Department> Department { get; set; }
+    }
+}
---
+++ DatabaseMVC/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseMVC.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+
+        public string DeptName { get; set; }
+
+        public string Location { get; set; }
+
+        public int NumberOfStaffs { get; set; }
+
+    }
+}
